0869b0b899ad8917fd4b5db176ddc53ba48244b5
novilfahlevy <novilfreon@gmail.com>
2025-01-28 21:26:57 +0800

    pakai state

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,34 @@ void main() {
   runApp(const TaskApp());
 }
 
-class TaskApp extends StatelessWidget {
+class TaskApp extends StatefulWidget {
   const TaskApp({super.key});
+
+  @override
+  State<TaskApp> createState() => _TaskAppState();
+}
+
+class _TaskAppState extends State<TaskApp> {
+  List<Map<String, String>> tugas = [];
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  TextEditingController tugasController = TextEditingController();
+
+  TextEditingController deskripsiController = TextEditingController();
+
+  void tambahTugas(String tugas, String deskripsi) {
+    if (formKey.currentState!.validate()) {
+      setState(() {
+        this.tugas.add({
+          'tugas': tugas,
+          'deskripsi': deskripsi,
+        });
+      });
+    } 
+  }
+
+  void hapusTugas(int index) => setState(() => tugas.removeAt(index));
 
   @override
   Widget build(BuildContext context) {
@@ -16,82 +42,77 @@ class TaskApp extends StatelessWidget {
           title: const Text('TaskApp', style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.blue,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Tugas',
-                  hintText: 'Contoh: Lari pagi',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )
+        body: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: tugasController,
+                  decoration: InputDecoration(
+                    labelText: 'Tugas',
+                    hintText: 'Contoh: Lari pagi',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )
+                  ),
+                  validator: (value) => value!.isEmpty ? 'Tidak boleh kosong' : null,
                 ),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Deskripsi',
-                  hintText: 'Contoh: 20 KM',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: deskripsiController,
+                  decoration: InputDecoration(
+                    labelText: 'Deskripsi',
+                    hintText: 'Contoh: 20 KM',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )
+                  ),
+                  validator: (value) => value!.isEmpty ? 'Tidak boleh kosong' : null,
                 ),
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(20),
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      tambahTugas(tugasController.text, deskripsiController.text);
+                      tugasController.clear();
+                      deskripsiController.clear();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(20),
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text('Tambah'),
+                  ),
+                ),
+          
+                const SizedBox(height: 50),
+          
+                for (int i = 0; i < tugas.length; i++)
+                  ListTile(
+                    title: Text(tugas[i]['tugas']!),
+                    subtitle: Text(tugas[i]['deskripsi']!),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () => hapusTugas(i),
+                    ),
                     shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  child: const Text('Tambah'),
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              ListTile(
-                title: const Text('Lari pagi'),
-                subtitle: const Text('Target minimal 20 KM'),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {},
-                ),
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(
-                    color: Colors.blue,
-                    width: 2,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              ListTile(
-                title: const Text('Ngerjain Post-test Flutter'),
-                subtitle: const Text('Estimasi ngerjain 2 jam'),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {},
-                ),
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(
-                    color: Colors.blue,
-                    width: 2,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ],
+                  const SizedBox(height: 10),
+              ],
+            ),
           ),
         ),
       ),
